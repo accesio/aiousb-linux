d2fd31976c149b4f1babff858fde7a1048906066
John Hentges <JHentges@accesio.com>
2020-11-10 09:58:46 -0800

    Updated DIO sample

--- samples/AiousbDioSample.cpp
+++ samples/AiousbDioSample.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstring>
 #include <list>
 #include <algorithm>
@@ -19,6 +20,8 @@ int main (int argc, char **argv)
   uint32_t Pid;
   bool PerBitControl = false;
 
+  std::cout<<"ACCES AIOUSB-Linux DIO sample"<<std::endl;
+
   aiousb_init();
 
   //Try to get a device handle.
@@ -28,17 +31,23 @@ int main (int argc, char **argv)
   {
     std::cout << "Unable to get device handle" << std::endl;
     SampleUsage(argc, argv);
+    exit (-1);
   }
 
-  //DIO bytes is something we can query from the aiousb library so we see how
-  //many are available on this device.
-  aiousb_query_device_info(Device, &Pid, nullptr, nullptr, &DioBytes, nullptr);
+  uint32_t nameSize = 255;
+  char name[nameSize];
+
+  // DIO bytes is something we can query from the aiousb library so we see how
+  // many are available on this device.
+  aiousb_query_device_info(Device, &Pid, &nameSize, name, &DioBytes, nullptr);
 
   if ( 0 == DioBytes )
   {
-    std::cout << "Device doesn't support DIO" << std::endl;
+    std::cout << name << " doesn't support DIO" << std::endl;
     exit (-1);
   }
+
+  std::cout << name << " detected, with " << DioBytes << " digital bytes" << std::endl;
 
   //Check if this device has direction control per bit.
   if (std::find(PIDS_WITH_PER_BIT.begin(), PIDS_WITH_PER_BIT.end(), Pid) != PIDS_WITH_PER_BIT.end())
@@ -50,15 +59,13 @@ int main (int argc, char **argv)
   //  on devices that don't have per bit control
   uint8_t *Config;
   size_t ConfigBytes = PerBitControl ? DioBytes : DioBytes % 8 + 1;
-  //Data is value of the bits read or written
+  // Data is value of the bits read or written
   uint8_t *Data;
-
-
 
   Config = new uint8_t[ConfigBytes];
   Data = new uint8_t[DioBytes];
 
-  //First we'll set all the bits for output and toggle them a couple of times.
+  // First we'll set all the bits for output and toggle them a couple of times.
   memset(Config, 0xff, ConfigBytes);
   memset(Data, 0xff, DioBytes);
 
@@ -84,20 +91,22 @@ int main (int argc, char **argv)
   aiousb_dio_configure(Device, false, Config, Data);
   aiousb_dio_read_all(Device, Data);
 
+  std::cout << "Read: ";
   for (unsigned int i = 0 ; i < DioBytes ; i++)
   {
-    std::cout << std::hex << "0x" << int(Data[i]) << " ";
+    std::cout << "bits "<< std::dec << i*8 << "-" << (i+1)*8-1 << ": 0x" << std::hex << std::setw(2) << std::setfill('0') << int(Data[i]) << "   ";
   }
   std::cout << std::endl;
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+  std::cout << "Sleeping for 5 seconds; change the input state before 2nd read." << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
   std::cout << "Performing second read" << std::endl;
   aiousb_dio_read_all(Device, Data);
-
+  std::cout << "Read: ";
   for (unsigned int i = 0 ; i < DioBytes ; i++)
   {
-    std::cout << std::hex << "0x" << int(Data[i]) << " ";
+    std::cout << "bits "<< std::dec << i*8 << "-" << (i+1)*8-1 << ": 0x" << std::hex << std::setw(2) << std::setfill('0') << int(Data[i]) << "   ";
   }
   std::cout << std::endl;
 
