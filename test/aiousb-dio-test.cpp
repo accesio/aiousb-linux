2f2c9c02e30a5e53726fa281d12dbaa79d359e92
Jay Dolan <jay.dolan@accesio.com>
2021-05-23 20:38:22 -0700

    Rework dio test
    * Rework DIO test for different categories and more complete coverage

--- test/aiousb-dio-test.cpp
+++ test/aiousb-dio-test.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <iomanip>
 
 #include <map>
 
@@ -14,23 +15,14 @@
 #include "AiousbSamples.inc"
 
 //24 bit cards are in 32 group
-enum BitGroup {BitGroup12, BitGroup16, BitGroup32, BitGroup32i, BitGroup48_96};
+enum TestGroup {TestGroup32, TestGroup32i, TestGroup48_96, TestGroupNone};
 
-std::map<uint16_t, BitGroup> PidToGroup =
+std::map<std::string, TestGroup> TestToGroup =
 {
-    { 0x8001, BitGroup32 },
-    { 0x0004, BitGroup32i },
-    { 0x8005, BitGroup32 },
-    { 0x8002, BitGroup48_96 },
-    { 0x8003, BitGroup48_96 },
-    { 0x8006, BitGroup32 },
-    { 0x800c, BitGroup16 },
-    { 0x800f, BitGroup16 },
-    { 0x8032, BitGroup16 }, //Probably shouldn't be here because it's an RO
-    { 0x804c, BitGroup32 },
-    { 0x803c, BitGroup32 },
-    { 0x8030, BitGroup12 },
-    { 0x803E, BitGroup12 },
+    { "./aiousb-dio32-test", TestGroup32 },
+    { "./aiousb-dio32i-test", TestGroup32i },
+    { "./aiousb-dio96-test", TestGroup48_96 },
+    { "./aiousb-dio-test", TestGroupNone },
 };
 
 #define err_printf(fmt, ...) \
@@ -41,8 +33,36 @@ int main (int argc, char **argv)
 {
     int Status;
     AIOUSB::aiousb_device_handle Device;
+    int DioBytes;
 
     AIOUSB::AiousbInit();
+
+    std::map<std::string, TestGroup>::iterator Group = TestToGroup.find(argv[0]);
+
+    switch (Group->second)
+    {
+        case TestGroup32:
+        std::cout << "TestGroup32" << std::endl;
+        DioBytes = 4;
+        break;
+
+        case TestGroup32i:
+        std::cout << "TestGroup32i" << std::endl;
+        DioBytes = 4;
+        break;
+
+        case TestGroup48_96:
+        std::cout << "TestGroup58_96" << std::endl;
+        DioBytes = 8;
+        break;
+
+        case TestGroupNone:
+        std::cout << "Run as bit specific test eg aiousb-dio-test aiousb-dio32-test" << std::endl;
+        exit(-1);
+        break;
+    };
+
+
 
     Status = SampleGetDeviceHandle(argc, argv, &Device);
 
@@ -51,35 +71,96 @@ int main (int argc, char **argv)
         err_printf("Unable to open device\n");
     }
 
+    uint8_t *OutMask= new uint8_t[DioBytes];
+    uint8_t *DioData = new uint8_t[DioBytes];
+    memset(OutMask, 0xff, DioBytes);
+    memset(DioData, 0xff, DioBytes);
+    Status = AIOUSB::DIO_Configure(Device, 0, OutMask, DioData);
+
+    std::cout << "Toggling bits with DIO_write1()" << std::endl;
+    for (int i = 0 ; i < 3 ; i++)
     {
-        unsigned char out_mask[4] = {0};
-        unsigned char dio_bytes[4] = {0};
-
-        memset(out_mask, 0xff, sizeof(out_mask));
-        memset(dio_bytes, 0xff, sizeof(dio_bytes));
-
-        Status = AIOUSB::DIO_Configure(Device, 0, out_mask, dio_bytes);
-
-        Status = AIOUSB::DIO_Write1(Device, 0, 0);
-        sleep(3);
-        Status = AIOUSB::DIO_Write1(Device, 0, 1);
-        sleep(3);
-        Status = AIOUSB::DIO_Write1(Device, 0, 0);
-        sleep(3);
-        Status = AIOUSB::DIO_Write1(Device, 0, 1);
+        for (int j = 0 ; j < DioBytes * 8 ; j++)
+        {
+            Status = AIOUSB::DIO_Write1(Device, j, 1);
+        }
+        sleep (3);
+        for (int j = 0 ; j < DioBytes * 8 ; j++)
+        {
+            Status = AIOUSB::DIO_Write1(Device, j, 0);
+        }
         sleep(3);
     }
 
+    std::cout << "Toggling bits with DIO_Write8()" << std::endl;
+    for (int i = 0 ; i < 3 ; i++)
     {
-        unsigned char dio_bytes[4] = {0};
-        unsigned char a_byte = 0x0;
-
-        Status = AIOUSB::DIO_ReadAll(Device, dio_bytes);
-
-        printf("break\n");
-
-        Status = AIOUSB::DIO_Read8(Device, 0, &a_byte);
-
-        printf("break\n");
+        for (int j = 0 ; j < DioBytes; j++)
+        {
+           Status = AIOUSB::DIO_Write8(Device, j, 0xff);
+        }
+        sleep (3);
+        for (int j = 0 ; j < DioBytes * 8 ; j++)
+        {
+            Status = AIOUSB::DIO_Write8(Device, j, 0x0);
+        }
+        sleep(3);
     }
+
+     std::cout << "Toggling bits with DIO_WriteAll()" << std::endl;
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        memset(DioData, 0xff, DioBytes);
+        for (int j = 0 ; j < DioBytes; j++)
+        Status = AIOUSB::DIO_WriteAll(Device, DioData);
+        sleep (3);
+        memset(DioData, 0xff, DioBytes);
+        Status = AIOUSB::DIO_WriteAll(Device, DioData);
+        sleep(3);
+    }
+
+    memset(OutMask, 0x00, DioBytes);
+    Status = AIOUSB::DIO_Configure(Device, 0, OutMask, DioData);
+
+    std::cout << "Reading inputs with DIO_Read1()" << std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        memset(DioData, 0, DioBytes);
+        for (int j = 0 ; j < DioBytes * 8 ; j++)
+        {
+            uint8_t Reading;
+            AIOUSB::DIO_Read1(Device, j, &Reading);
+            DioData[j/8] |= Reading << (j % 8);
+        }
+        sleep (3);
+        for (int j = 0 ; j < DioBytes; j++)
+        {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)DioData[j] << std::endl;
+        }
+    }
+    std::cout << "Reading inputs with DIO_Read8()" << std::endl;
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        for (int j = 0 ; j < DioBytes ; j++)
+        {
+            AIOUSB::DIO_Read8(Device, j, &(DioData[j]));
+        }
+        sleep(3);
+        for (int j = 0 ; j < DioBytes; j++)
+        {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)DioData[j] << std::endl;
+        }
+    }
+
+    std::cout << "Reading inputs with DIO_ReadAll()" << std::endl;
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        AIOUSB::DIO_ReadAll(Device, DioData);
+        sleep(3);
+        for (int j = 0 ; j < DioBytes; j++)
+        {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)DioData[j] << std::endl;
+        }
+    }
+
 }
