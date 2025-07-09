c8534466c969653408566e19d00e777462bc88e8
Jay Dolan <jay.dolan@accesio.com>
2025-07-08 20:24:45 -0700

    Update adc sample
    * Add differential mode to adc sample
    * Updates to tests and test README.md

--- samples/AiousbAdcSample.cpp
+++ samples/AiousbAdcSample.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+#include <getopt.h>
 
 #include "aiousb.h"
 #include "sample_helpers.inc"
@@ -12,7 +13,23 @@
 
 #include "AiousbSamples.inc"
 
-void DoChanScan(AIOUSB::aiousb_device_handle Device, int ChannelCount);
+struct option long_options[] = {
+  {"differential", no_argument, nullptr, 'd'},
+  {"help", no_argument, nullptr, 'h'},
+  {nullptr, 0, nullptr, 0}
+};
+
+void Usage ()
+{
+  std::cout << "Usage:" << std::endl;
+  std::cout << "AiousbAdcSample [--differential] [<device_file>]" << std::endl;
+  std::cout << "If no device file is specified and only one AIOUSB device is detected that lone device will be used" << std::endl;
+}
+
+int GetNumAdcMuxChan(uint32_t Pid);
+int GetNumAdcsImm(uint32_t Pid);
+
+void DoChanScan(AIOUSB::aiousb_device_handle Device, int ChannelCount, bool Differential);
 void DoImmScan(AIOUSB::aiousb_device_handle Device, int ChannelCount);
 
 int main (int argc, char **argv)
@@ -22,6 +39,27 @@ int main (int argc, char **argv)
   uint32_t Pid;
   int ChannelCount;
   uint64_t SerialNum;
+  bool Differential = false;
+
+  do
+  {
+    int opt = getopt_long(argc, argv, "", long_options, nullptr);
+    if (opt == -1) break;
+
+    switch (opt)
+    {
+      case 'd':
+        Differential = true;
+        break;
+      case 'h':
+        Usage();
+        exit(0);
+        break;
+      default:
+        Usage();
+        exit(-1);
+    }
+  } while (true);
 
   std::cout<<"ACCES AIOUSB-Linux ADC sample"<<std::endl;
 
@@ -46,40 +84,47 @@ int main (int argc, char **argv)
   std::cout << "Serial Number: " <<std::hex << SerialNum << std::dec << std::endl;
 
   ChannelCount = GetNumAdcMuxChan(Pid);
-  if (0 != ChannelCount) DoChanScan(Device, ChannelCount);
+  if (0 != ChannelCount)
+  {
+    std::cout << "This card supports timer based acquisition" << std::endl;
+    DoChanScan(Device, ChannelCount, Differential);
+  }
 
   ChannelCount = GetNumAdcsImm(Pid);
-  if (0 != ChannelCount) DoImmScan(Device, ChannelCount);
-
+  if (0 != ChannelCount)
+  {
+    std::cout << "This card supports immediate acquisition." << std::endl;
+    DoImmScan(Device, ChannelCount);
+  }
 
 }
 
 //USB-AI style boards
-void DoChanScan(AIOUSB::aiousb_device_handle Device, int ChannelCount)
+void DoChanScan(AIOUSB::aiousb_device_handle Device, int ChannelCount, bool Differential)
 {
   int Status;
   double Volts[ChannelCount];
   uint32_t ConfigSize = 21;
   uint8_t Config[ConfigSize];
   int EndChannel = ChannelCount - 1;
-
+  uint8_t ChannelByte = Differential ? 0x1 : 0x09;
   //Generate a sample config to do a scan of all channels
-  Config[0x00] = 0x01; // range
-  Config[0x01] = 0x01; // range
-  Config[0x02] = 0x01; // range
-  Config[0x03] = 0x01; // range
-  Config[0x04] = 0x01; // range
-  Config[0x05] = 0x01; // range
-  Config[0x06] = 0x01; // range
-  Config[0x07] = 0x01; // range
-  Config[0x08] = 0x01; // range
-  Config[0x09] = 0x01; // range
-  Config[0x0A] = 0x01; // range
-  Config[0x0B] = 0x01; // range
-  Config[0x0C] = 0x01; // range
-  Config[0x0D] = 0x01; // range
-  Config[0x0E] = 0x01; // range
-  Config[0x0F] = 0x01; // range
+  Config[0x00] = ChannelByte; // range
+  Config[0x01] = ChannelByte; // range
+  Config[0x02] = ChannelByte; // range
+  Config[0x03] = ChannelByte; // range
+  Config[0x04] = ChannelByte; // range
+  Config[0x05] = ChannelByte; // range
+  Config[0x06] = ChannelByte; // range
+  Config[0x07] = ChannelByte; // range
+  Config[0x08] = ChannelByte; // range
+  Config[0x09] = ChannelByte; // range
+  Config[0x0A] = ChannelByte; // range
+  Config[0x0B] = ChannelByte; // range
+  Config[0x0C] = ChannelByte; // range
+  Config[0x0D] = ChannelByte; // range
+  Config[0x0E] = ChannelByte; // range
+  Config[0x0F] = ChannelByte; // range
   Config[0x10] = 0x00; // calibration code
   Config[0x11] = 0x05; // trigger & counter mode
 
@@ -107,6 +152,7 @@ void DoChanScan(AIOUSB::aiousb_device_handle Device, int ChannelCount)
 }
 
 //USB-AO boards with immediete ADCs
+// These boards have a fixed gain.
 void DoImmScan(AIOUSB::aiousb_device_handle Device, int ChannelCount)
 {
   int Status;
